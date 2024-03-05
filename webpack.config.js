4c4b46c2b3cda91f97e79cba28a7a76a3397f6b0
SerDunk <abhinavkondapalli7@gmail.com>
2024-03-05 22:48:41 +0530

    Basic setup with navbar code

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path=require('path')
+
+module.exports={
+    entry:'./src/index.js',
+    output:{
+        filename:'main.js',
+        path:path.resolve(__dirname,'dist'),
+    },
+    mode:'development',
+
+    devServer:{
+        static:'dist',
+        port:3000,
+        open:true,
+    },
+
+    module: {
+        rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        ],
+    },
+
+
+};
